8d2b9007c60c2b3c5298c628abe5d123764aa326
sergr972 <sergr972@gmail.com>
2023-08-25 15:20:37 +0300

    add Brackets and Fibonacci

---
+++ src/main/java/nterview/tasks/Fibonacci.java
@@ -0,0 +1,30 @@
+package main.java.nterview.tasks;
+
+import java.util.stream.Stream;
+
+public class Fibonacci {
+    private static final int NUM = 10;
+
+    public static void main(String[] args) {
+        System.out.println(getFibonacciValue(NUM));
+        System.out.println(sumFibonacciValue());
+    }
+
+    public static int getFibonacciValue(int n) {
+        if (n <= 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else  {
+            return getFibonacciValue(n - 1) + getFibonacciValue(n - 2);
+        }
+    }
+
+    private static int sumFibonacciValue() {
+        return Stream.iterate(new int[]{0, 1}, arr -> new int[]{arr[1], arr[0] + arr[1]})
+                .limit(Fibonacci.NUM)
+                .map(t -> t[0])
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+}
